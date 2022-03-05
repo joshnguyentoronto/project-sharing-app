e5bbf8e8637c8cf99f4c7ab922ff838308b3a2fd
Lu Ye <alison94713@gmail.com>
2022-03-04 21:53:17 -0500

    updated profile

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -4,27 +4,40 @@ import { Link } from 'react-router-dom';
 
 export default class ProfileCard extends Component {
     state = {
-        name: "Lu",
+        name: "Lu Ye",
         location: "Ontario",
-        viewCount: 2,
-        likeCount: 3,
-        bio: "Hello"
+        viewCount: 0,
+        likeCount: 0,
+        bio: "fsdf",
+        email: "luye@luye.com",
+        users: []
     }
+
+    // async componentDidMount() {
+    //     try {
+    //         let fetchUserList = await fetch('/api/users')
+    //         console.log(fetchUserList)
+    //         let users = await fetchUserList.json()
+    //         console.log(users)
+    //         // this.setState({users: users})
+    //     } catch(err) {
+    //         console.log("profile card error: ", err)
+    //     }
+    // }
 
     render () {
         return (
-            <div>
+            <div className="profile-card" >
                 <img src="#" alt="profile"/>
                 <h1>{this.state.name}</h1>
-                <p>{this.state.location}</p>
+                <h4>{this.state.location}</h4>
                 <Link to="/profile/edit">Edit Profile</Link>
                 <br />
                 <Link to="/upload">Upload a Project</Link>
-                <p>Project Views {this.state.viewCount}</p>
-                <p>Likes {this.state.likeCount}</p>
-                <p>Contact</p>
-                <p>About</p>
-                <p>{this.state.bio}</p>
+                <p><span>Project Views</span> <span>{this.state.viewCount}</span></p>
+                <p><span>Likes</span> <span>{this.state.likeCount}</span></p>
+                <p><span className="profile-label">Contact</span>{this.state.email}</p>
+                <p><span className="profile-label">About</span>{this.state.bio}</p>
             </div>
         )
     }
